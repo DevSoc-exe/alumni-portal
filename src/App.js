bca010c6abcfde73953340a4e1f12201dd42384e
Tushar Singh <tushar23123@gmail.com>
2023-10-21 23:56:35 +0530

    small error fixed in linking login signup page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <React.Fragment>
       <Routes>
-        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
       </Routes>
     </React.Fragment>
