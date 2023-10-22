f35b28419081b3e29b1fba166d940a4400546a20
charankamal20 <charanksingh20@gmail.com>
2023-10-22 15:08:49 +0530

    Corrected Iframe issue

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import './App.css';
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 
-import { Forum } from "./pages/forum"
 import { Login } from "./pages/login"
 import { Signup } from "./pages/signup"
+import Forum from './pages/forum';
 
 function App() {
   return (
     <React.Fragment>
       <Routes>
         <Route path='/' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
         <Route path='/forum' element={<Forum />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
     </React.Fragment>
   );
