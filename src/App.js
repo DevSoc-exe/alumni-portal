c746e7baa7adfae25564ab8cb1ae942ea16ed586
charankamal20 <charanksingh20@gmail.com>
2023-10-25 12:50:02 +0530

    Creted header and forum

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
-
 import { Login } from "./pages/login"
 import { Signup } from "./pages/signup"
 import Forum from './pages/forum';
 
 function App() {
+  
+  const currentUser = {name: 'John Doe', userImage: "", email: 'johnDoe@gmail.com', designation: "Senior Product Developer", company: "Google"};
+  const posts = [{creatorName: "John Doe", designation: "Senior Product Developer", company: "Google", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", likes: 20, image: ""},];
+
   return (
     <React.Fragment>
       <Routes>
         <Route path='/' element={<Login />} />
-        <Route path='/forum' element={<Forum />} />
+        <Route path='/forum' element={<Forum posts={posts} currUser={currentUser}/>} />
         <Route path='/signup' element={<Signup />} />
       </Routes>
     </React.Fragment>
--- src/pages/forum/index.jsx
+++ src/pages/forum/index.jsx
@@ -1,13 +1,37 @@
 // react
 import React from 'react';
+import Header from '../../components/header';
+import Post from '../../components/post';
+import ProfileCard from '../../components/profileCard';
 import './style.css'
 
-export default function Forum() {
+export default function Forum({currUser, posts}) {
 
     return (
 
-        <div id='background' className=' bg-green-500 text-white h-screen border border-black w-full z-80'>
-            <h1 className='text-white'>This is the fourm</h1>
+        <div id='background' className='bg-lightGrey h-screen border border-black w-full z-80'>
+            <Header 
+                // user_firstName={currUser.name}
+                user_firstName="John Doe"
+            />
+            <div className='grid grid-cols-12 gap-2 w-10/12'>
+                 <div className='col-span-3 border rounded-lg border-black'>
+                    <ProfileCard />
+                 </div>
+                 <div className='col-span-6 rounded-lg'>
+                    {
+                        posts.map(t => (
+                            <Post
+                                // key={t.uuid} 
+                                post={t}
+                            />
+                        ))
+                    }
+                 </div>
+                 <div className='col-span-3 border border-black rounded-lg'>
+                    Create Post
+                 </div>
+            </div>            
         </div>
     );
 }
